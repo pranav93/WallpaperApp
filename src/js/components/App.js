b1c36d136ddb26ab0edd6eb0908375e39cb1a22d
Pranav <ambhore.pranav@gmail.com>
2021-11-22 13:58:38 +0530

    Show random wallpaper

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,16 +1,32 @@
 import React from "react";
+import { useState } from "react";
 
-const App = () => (
-  <>
-    <h1>App Component</h1>
-    <button
-      onClick={() => {
-        electron.notificationApi.sendNotification("My custom notification");
-      }}
-    >
-      Notify
-    </button>
-  </>
-);
+const Picture = ({ data }) => <img src={data} />;
+
+const App = () => {
+  const [pictureData, setPictureData] = useState("");
+  return (
+    <>
+      <h1>Wallpaper</h1>
+      <button
+        onClick={async () => {
+          const imageUrl = "https://source.unsplash.com/1600x900/?wallpaper";
+          const response = await fetch(imageUrl);
+          const imageBlob = await response.blob();
+          const reader = new FileReader();
+          reader.readAsDataURL(imageBlob);
+          reader.onloadend = () => {
+            const base64data = reader.result;
+            console.log(base64data);
+            setPictureData(base64data);
+          };
+        }}
+      >
+        New wallpaper
+      </button>
+      <Picture data={pictureData}></Picture>
+    </>
+  );
+};
 
 export default App;
