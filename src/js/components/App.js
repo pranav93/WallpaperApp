2be0fc7093e91a7e9aa7ca55e8b43c1fea086942
Pranav <ambhore.pranav@gmail.com>
2021-11-28 12:48:03 +0530

    Refactored code

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -9,6 +9,33 @@ const Picture = ({ className, data }) => (
 const App = () => {
   const [pictureData, setPictureData] = useState("");
   const [pictureName, setPictureName] = useState("");
+
+  const downloadWallpaper = async () => {
+    // const imageUrl = "https://source.unsplash.com/1600x900/?wallpaper";
+    const imageUrl =
+      "https://images.unsplash.com/photo-1451337516015-6b6e9a44a8a3?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8d2FsbHBhcGVyfHx8fHx8MTYzODA4MjI4OA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600";
+    electron.notificationApi.sendNotification("Getting wallpaper");
+    const response = await fetch(imageUrl);
+    const imageBlob = await response.blob();
+    const photoId = await response.url.match(/photo.*\?/g)[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(imageBlob);
+    reader.onloadend = () => {
+      const base64data = reader.result;
+      console.log(base64data);
+      setPictureData(base64data);
+      setPictureName(photoId);
+      electron.notificationApi.sendNotification("Done");
+    };
+  };
+
+  const setWallpaper = () => {
+    electron.filesApi.saveFile(pictureData, pictureName);
+    electron.notificationApi.sendNotification("Saved");
+    electron.shellApi.setWallpaper(pictureName);
+    electron.notificationApi.sendNotification("Applied wallpaper");
+  };
+
   return (
     <div className={styles.AppContainer}>
       {/* <h1 className={styles.CustomCss}>Wallpaper</h1> */}
@@ -16,38 +43,8 @@ const App = () => {
         <Picture className={styles.Picture} data={pictureData}></Picture>
       </div>
       <div className={styles.ActionContainer}>
-        <button
-          onClick={async () => {
-            // const imageUrl = "https://source.unsplash.com/1600x900/?wallpaper";
-            const imageUrl =
-              "https://images.unsplash.com/photo-1451337516015-6b6e9a44a8a3?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8d2FsbHBhcGVyfHx8fHx8MTYzODA4MjI4OA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600";
-            electron.notificationApi.sendNotification("Getting wallpaper");
-            const response = await fetch(imageUrl);
-            const imageBlob = await response.blob();
-            const photoId = await response.url.match(/photo.*\?/g)[0];
-            const reader = new FileReader();
-            reader.readAsDataURL(imageBlob);
-            reader.onloadend = () => {
-              const base64data = reader.result;
-              console.log(base64data);
-              setPictureData(base64data);
-              setPictureName(photoId);
-              electron.notificationApi.sendNotification("Done");
-            };
-          }}
-        >
-          New wallpaper
-        </button>
-        <button
-          onClick={() => {
-            electron.filesApi.saveFile(pictureData, pictureName);
-            electron.notificationApi.sendNotification("Saved");
-            electron.shellApi.setWallpaper(pictureName);
-            electron.notificationApi.sendNotification("Applied wallpaper");
-          }}
-        >
-          Set wallpaper
-        </button>
+        <button onClick={downloadWallpaper}>New wallpaper</button>
+        <button onClick={setWallpaper}>Set wallpaper</button>
       </div>
     </div>
   );
