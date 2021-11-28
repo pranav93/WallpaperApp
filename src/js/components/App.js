d6cbc52ed18f430bc3e34aead6ae9483da7f894c
Pranav <ambhore.pranav@gmail.com>
2021-11-28 13:08:52 +0530

    Refactored code

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -4,8 +4,8 @@ import styles from "./App.module.css";
 import Display from "./Display";
 
 const App = () => {
-  const [pictureData, setPictureData] = useState("");
-  const [pictureName, setPictureName] = useState("");
+  const [wallpaperData, setWallpaperData] = useState("");
+  const [wallpaperName, setWallpaperName] = useState("");
 
   const downloadWallpaper = async () => {
     const imageUrl = "https://source.unsplash.com/1600x900/?wallpaper";
@@ -20,23 +20,23 @@ const App = () => {
     reader.onloadend = () => {
       const base64data = reader.result;
       console.log(base64data);
-      setPictureData(base64data);
-      setPictureName(photoId);
+      setWallpaperData(base64data);
+      setWallpaperName(photoId);
       electron.notificationApi.sendNotification("Done");
     };
   };
 
   const setWallpaper = () => {
-    electron.filesApi.saveFile(pictureData, pictureName);
+    electron.filesApi.saveFile(wallpaperData, wallpaperName);
     electron.notificationApi.sendNotification("Saved");
-    electron.shellApi.setWallpaper(pictureName);
+    electron.shellApi.setWallpaper(wallpaperName);
     electron.notificationApi.sendNotification("Applied wallpaper");
   };
 
   return (
     <div className={styles.AppContainer}>
       {/* <h1 className={styles.CustomCss}>Wallpaper</h1> */}
-      <Display wallpaperData={pictureData} />
+      <Display wallpaperData={wallpaperData} />
       <div className={styles.ActionContainer}>
         <button onClick={downloadWallpaper}>New wallpaper</button>
         <button onClick={setWallpaper}>Set wallpaper</button>
